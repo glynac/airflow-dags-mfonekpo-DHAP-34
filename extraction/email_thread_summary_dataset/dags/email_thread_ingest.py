f2a714a8e99decc20d37723a5d9f109fad2305ab
mfonekpo <mfonekpo34@gmail.com>
2026-01-09 22:04:10 +0100

    include docstrings for dags file

--- extraction/email_thread_summary_dataset/dags/email_thread_ingest.py
+++ extraction/email_thread_summary_dataset/dags/email_thread_ingest.py
@@ -9,10 +9,14 @@ from validation import validation
     schedule="@once",
 )
 def email_thread_ingest_dag():
+    """
+    This DAG is responsible for ingesting the email thread summary dataset.
+    It consists of two tasks: file_check and validation.
+    The file_check task checks if the necessary files exist in the right location.
+    The validation task validates the schema of the ingested data against the expected schema.
+    """
     file_check_task = file_check()
     validation_task = validation()
-
-
 
     file_check_task >> validation_task
 
